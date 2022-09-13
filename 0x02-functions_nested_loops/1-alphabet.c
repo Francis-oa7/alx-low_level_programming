04a07afb809af5eaebbe898ed348fc40298b2bb1
Francis-oa7 <fomaneasirifi@gmail.com>
2022-09-13 23:07:09 +0000

    Added a C file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - prints lowercase alphabets
+ */
+
+void print_alphabet(void)
+{
+	char alph;
+	
+	for(alph = 'a'; alph <= 'z'; alph++)
+	{
+		_putchar(alph);
+	}
+	_putchar('\n');
+}
