c3fcc09dd276b2afbb2adb2c0f3c91013fe367f1
Francis-oa7 <fomaneasirifi@gmail.com>
2022-09-14 02:02:40 +0000

    Added a C file

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
  * _abs - prints the absolute value of number
- * Return: null
+ * Return: absolute number of the input digit
  * @c: number to absolute
  */
 int _abs(int c)
 {
-	if (c > 0)
+	if (c >= 0)
 		return (c);
 	else
 		return (c * -1);
