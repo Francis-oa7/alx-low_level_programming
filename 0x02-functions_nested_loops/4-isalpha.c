aab536a8e46da8f617ec56fe042844774bcbaf6a
Francis-oa7 <fomaneasirifi@gmail.com>
2022-09-14 01:16:24 +0000

    Added a C file

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,11 @@
 #include "main.h"
-
 /**
  * _isalpha - checks if value is an alphabeth
- * 
+ *
  * @c: Number to be checked
- * 
+ *
  * Return: 1 on (Success) Or 0 (Failure)
  */
-
 int _isalpha(int c)
 {
 	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
