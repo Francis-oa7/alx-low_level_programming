be5bc6aa6b4dd267ccaaec06424555e697825f98
Francis-oa7 <fomaneasirifi@gmail.com>
2022-09-14 22:37:46 +0000

    Added a C file

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,10 +1,12 @@
 #include "main.h"
 /**
- *
-
+ * add - prints the sum of figure
+ * @a: number to be summed
+ * @b: number to be summed
+ * Return: null
+ */
 int add(int a, int b)
 {
 	int res = a + b;
-	_putchar(res);
-	
+	return (res);
 }
