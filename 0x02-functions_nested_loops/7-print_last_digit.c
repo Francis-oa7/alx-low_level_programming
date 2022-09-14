e0c8c6c65e04432ef8710f0bb34610730371586e
Francis-oa7 <fomaneasirifi@gmail.com>
2022-09-14 02:19:41 +0000

    Added a C file

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,10 +10,10 @@ int print_last_digit(int c)
 
 	if (c < 0)
 	{
-		last_d = (c * 10) * -1;
+		last_d = (c % 10) * -1;
 	}
 	else
-		last_d = c * 10;
+		last_d = c % 10;
 	_putchar(last_d + '0');
 	return (last_d);
 }
