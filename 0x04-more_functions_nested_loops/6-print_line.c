be6e5e1b7ada89d79ce0d9c034343610638b2525
Francis-oa7 <fomaneasirifi@gmail.com>
2022-09-17 03:42:52 +0000

    Added a C file

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_line - prints a line
+ * @n: number 0f -
+ * Return: nothing please
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n < 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar(95);
+		}
+		_putchar('\n');
+	}
+}
