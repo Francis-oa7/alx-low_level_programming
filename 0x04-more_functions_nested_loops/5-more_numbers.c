7a830ddad7fe24d1963c3c46dcd019d91b5c2f55
Francis-oa7 <fomaneasirifi@gmail.com>
2022-09-17 03:30:22 +0000

    Added a C file

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * more_numbers - prints ten times 0-14
+ * Return: does not return
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
