6ac147958743e5a148e229d62e7bf48024e379e2
Francis-oa7 <fomaneasirifi@gmail.com>
2022-09-20 22:36:46 +0000

    Added a C file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_t0_98 - changes value to 98
+ * @n: pointer to number to be changed
+ * Return: null
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,5 +3,5 @@
 int _putchar(int c);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
-
+void reset_to_98(int *n);
 #endif
