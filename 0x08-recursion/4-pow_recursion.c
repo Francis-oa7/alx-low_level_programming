78c00231000bb89492654c16d1de05859a0b7452
Francis-oa7 <fomaneasirifi@gmail.com>
2022-09-28 17:31:18 +0000

    Added a cfile

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - returns the exponent of number
+ * @x: base number
+ * @y: exponent number
+ * Return: x raised to the power y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y > 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
