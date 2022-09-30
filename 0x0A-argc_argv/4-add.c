60e330dd5ae13c3113f4b31ab4e6df962985fdb9
Francis-oa7 <fomaneasirifi@gmail.com>
2022-09-30 23:27:24 +0000

    Added a C file

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,33 +1,41 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
 /**
- * main - prints the sum of numbers
- * @argc: number of arguments
- * @argv: array of arguments
- * Return: 0 on success 1 otherwise
+ * checker - checks for valid input
+ * @argc: argument count
+ * @i: counter for argv[]
+ * @j: counter for argv[][]
+ * @argv: argument vector
+ * Return: 0 on success, 1 on failure
  */
-
+int checker(int argc, int i, unsigned int j, char *argv[])
+{
+	for (i = 1; i <= argc; i++)
+		for (j = 0; argv[i] != '\0' && j < strlen(argv[i]); j++)
+			if (isdigit(argv[i][j]) == 0)
+				return (1);
+	return (0);
+}
+/**
+ * main - adds all arguments together if they are digits.
+ * @argc: argument count only accepts ints separated by spaces.
+ * @argv: argument vector
+ * Return: 0 on success, 1 on failure
+ */
 int main(int argc, char *argv[])
 {
-	int i, sum;
+	int result, i;
 
-	sum = 0;
-
-	if (argc > 1)
+	result = 0;
+	if (checker(argc, 1, 0, argv) == 1)
 	{
-		for (i = 1; i < argc; i++)
-		{
-			if (isdigit((atoi(argv[i]))) == 1)
-			{
-				printf("Error\n");
-				return (1);
-			}
-			sum += atoi(argv[i]);
-		}
-		printf("%d\n", sum);
-		return (0);
+		printf("Error\n");
+		return (1);
 	}
-	else if (argc == 1)
-		printf("0\n");
+	for (i = 1; i < argc; i++)
+		result += atoi(argv[i]);
+	printf("%d\n", result);
+	return (0);
 }
