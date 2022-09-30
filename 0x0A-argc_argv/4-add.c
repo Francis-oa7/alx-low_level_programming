5fd80f1e57398036db50145443b4c82c10c8b7e1
Francis-oa7 <fomaneasirifi@gmail.com>
2022-09-30 23:18:52 +0000

    Added a C file

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -18,14 +18,16 @@ int main(int argc, char *argv[])
 	{
 		for (i = 1; i < argc; i++)
 		{
-			if (isdigit(argv[i]) == 0)
-				printf("Error");
+			if (isdigit((int) argv[i]) == 0)
+			{
+				printf("Error\n");
 				return (1);
+			}
 			sum += atoi(argv[i]);
 		}
 		printf("%d\n", sum);
 		return (0);
 	}
 	else if (argc == 1)
-		printf("0");
+		printf("0\n");
 }
