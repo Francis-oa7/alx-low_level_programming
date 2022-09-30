0c3e4bdc56607997590d5f3137e19d57b7620c30
Francis-oa7 <fomaneasirifi@gmail.com>
2022-09-30 21:44:59 +0000

    Added a C file

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - the main function
+ * @argc: argument count
+ * @argv: array of arguments
+ * Return: 0 on success 1 otherwise
+ */
+
+int main(int argc, char __attribute__ ((unused)) *argv[])
+{
+printf("%d\n", argc - 1);
+return (0);
+}
