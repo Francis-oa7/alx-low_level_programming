f8104cfdeb752f3b3c1dd3d06b6b2f50c97b12d7
Francis-oa7 <fomaneasirifi@gmail.com>
2022-09-30 21:55:13 +0000

    Added a C file

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ * @argc: argument count
+ * @argv: array of arguments
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+int i;
+if (argc > 1)
+{
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+return (0);
+}
